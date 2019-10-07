33a4f9262690ebf7b122f6eec55eb2987f1317d9
Jason Miller <developit@google.com>
2019-10-06 23:09:51 -0400

    test updates

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import path from 'path';
 /**
  * babel-preset-optimize is a preset that transforms unweildy source into something bundlers can optimize.
  * @param {Object} [options]
- * @param {boolean} [options.loose=false] Loose mode skips seldom-used transforms, like fixing function.name inference in Edge.
+ * @param {boolean} [options.loose=false] Loose mode enables transforms that produce smaller output but may cause issues with certain code.
  */
-export default function babelPresetModules(options = {}) {
-	// const loose = options.loose === true;
+export default function babelPresetModules(_, options = {}) {
+	const loose = options.loose === true;
 
 	return {
 		plugins: [
-			path.resolve(__dirname, './plugins/transform-umd-commonjs')
+			path.resolve(__dirname, './plugins/transform-umd-to-cjs'),
+			path.resolve(__dirname, './plugins/transform-cjs-to-esm'),
+			[path.resolve(__dirname, './plugins/transform-arguments'), { loose }],
+			path.resolve(__dirname, './plugins/transform-array-spread')
 		].filter(Boolean)
 	};
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,10 @@
 import { babel, dent } from './_util';
-import preset from '..';
+import preset from '../src';
 
-function babelPretty(code) {
-	return babel(code, { presets: [preset], compact: false, retainLines: true });
+function babelPretty(code, loose) {
+	return babel(code, { presets: [
+		[preset, { loose }]
+	], compact: false });
 }
 
 describe('babel-preset-optimize', () => {
@@ -14,9 +16,55 @@ describe('babel-preset-optimize', () => {
 		});
 	});
 
-	it('should pass a smoke test', () => {
+	it('should do nothing to already-modern code', () => {
 		expect(babelPretty(dent`
 			const x = 1;
-		`)).toMatchSnapshot();
+		`)).toMatchInlineSnapshot(`"const x = 1;"`);
+
+		const code = dent`
+			import foo from 'foo';
+			export default foo;
+		`;
+		expect(babelPretty(code)).toEqual(code);
+	});
+
+	it('should pass a smoke test', () => {
+		const code = dent`
+			(function(f) {
+				if (typeof module !== 'undefined' && module.exports) {
+					module.exports = f(require('dep'));
+				}
+			})(function(d) {
+				return function lib(opts) {
+					opts = opts || {};
+					return d.create(Object.assign({}, opts, {
+						isLib: true
+					}));
+				}
+			})
+		`;
+
+		expect(babelPretty(code)).toMatchInlineSnapshot(`
+			"var _default;
+			import d from \\"dep\\";
+			_default = function lib(opts) {
+			  opts = opts || {};
+			  return d.create(Object.assign({}, opts, {
+			    isLib: true
+			  }));
+			};
+			export default _default;"
+		`);
+
+		expect(babelPretty(code, true)).toMatchInlineSnapshot(`
+			"var _default;
+			import d from \\"dep\\";
+			_default = function lib(opts = {}) {
+			  return d.create(Object.assign({}, opts, {
+			    isLib: true
+			  }));
+			};
+			export default _default;"
+		`);
 	});
 });
