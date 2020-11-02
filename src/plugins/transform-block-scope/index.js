4f5f629416799a6cc41d8ac7c7e200103952be87
Jason Miller <jason@developit.ca>
2020-11-01 23:09:38 -0500

    (not enabled) add transform-block-scope

---
+++ src/plugins/transform-block-scope/index.js
@@ -0,0 +1,32 @@
+/** @typedef {import('@babel/core')} babel */
+
+/**
+ * Infer `let` usage from `var.
+ * @param {babel} api
+ * @returns {babel.PluginObj}
+ */
+export default function({ types: t }) {
+	return {
+		name: 'transform-block-scope',
+		visitor: {
+			VariableDeclaration(path) {
+				if (path.node.kind !== 'var') return;
+
+				const idents = path.getBindingIdentifierPaths();
+				for (const name in idents) {
+					const p = idents[name];
+					const b = p.scope.getBinding(name);
+					// unknown binding / free global / non-constant
+					if (!b || !b.constant) return;
+					// TDZ violations
+					for (const ref of b.referencePaths) {
+						if (ref !== p && ref.willIMaybeExecuteBefore(p)) {
+							return;
+						}
+					}
+				}
+				path.node.kind = 'let';
+			}
+		}
+	};
+}
---
+++ src/plugins/transform-block-scope/index.test.js
@@ -0,0 +1,37 @@
+import { babel } from '../../../test/_util';
+import plugin from '.';
+
+const CONFIG = {
+	compact: false,
+	plugins: [plugin]
+};
+
+const b = code => babel(code, CONFIG);
+
+describe('transform-block-scope', () => {
+	it('should convert var to let', () => {
+		expect(b(`var a;`)).toMatchInlineSnapshot(`"let a;"`);
+		expect(b(`var a = 1;`)).toMatchInlineSnapshot(`"let a = 1;"`);
+		expect(b(`var a = 1; a;`)).toMatchInlineSnapshot(`
+			"let a = 1;
+			a;"
+		`);
+		expect(b(`var a = function(){ return a; }`)).toMatchInlineSnapshot(`
+		"let a = function () {
+		  return a;
+		};"
+	`);
+	});
+
+	it('should exclude var declarations that violate TDZ', () => {
+		expect(b(`a; var a = 1;`)).toMatchInlineSnapshot(`
+			"a;
+			var a = 1;"
+		`);
+		expect(b(`(a => { var a = 1; })();`)).toMatchInlineSnapshot(`
+		"(a => {
+		  var a = 1;
+		})();"
+	`);
+	});
+});
