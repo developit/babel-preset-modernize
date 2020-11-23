975b4eec6d343217f81f099790fb02ccfb6a0899
Jason Miller <jason@developit.ca>
2020-11-22 22:41:07 -0500

    Add simple test for transform-remove-polyfills

--- src/plugins/transform-remove-polyfills/index.test.js
+++ src/plugins/transform-remove-polyfills/index.test.js
@@ -8,6 +8,14 @@ const CONFIG = {
 
 describe('transform-remove-polyfills', () => {
 	it('should work', () => {
-		expect(babel(dent``, CONFIG)).toMatchInlineSnapshot(``);
+		expect(
+			babel(
+				dent`
+				function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+				var a = _extends({}, b);
+				`,
+				CONFIG
+			)
+		).toMatchInlineSnapshot(`"var a = Object.assign({}, b);"`);
 	});
 });
