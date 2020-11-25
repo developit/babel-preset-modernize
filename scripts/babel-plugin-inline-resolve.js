89ea0568234e34dc2efaef4aeb3f84a88550ac12
Jason Miller <jason@developit.ca>
2020-11-24 21:58:05 -0500

    license headers

--- scripts/babel-plugin-inline-resolve.js
+++ scripts/babel-plugin-inline-resolve.js
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Convert all require.resolve() and path.resolve() expressions in a module to require() expressions.
  *
  * @see https://astexplorer.net/#/gist/e0d4a9a500b48b472f130eed1cb67818/f3d4a5ceeb80e4f188c2850de3582116b9eff8cd
