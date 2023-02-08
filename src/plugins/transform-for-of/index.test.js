3d420a0b05836279500a6e2a5815e355569f7ef1
Jason Miller <developit@users.noreply.github.com>
2023-02-07 21:28:24 -0500

    Update index.test.js

--- src/plugins/transform-for-of/index.test.js
+++ src/plugins/transform-for-of/index.test.js
@@ -81,7 +81,7 @@ describe('transform-for-of', () => {
 								};
 							}
 
-							throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+							throw new TypeError("Invalid attempt to iterate non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
 						}
 					}	
 				`,
